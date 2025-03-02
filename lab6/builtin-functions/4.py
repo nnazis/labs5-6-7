b414c35b64b921978d3790ef8a08c092f998ef51
Nazira <nazira08.com@gmail.com>
2025-03-02 15:19:11 +0500

    Task 4

---
+++ lab6/builtin-functions/4.py
@@ -0,0 +1,7 @@
+import time
+import math
+num = int(input("Enter a number: "))
+milliseconds = int(input("Enter milliseconds: "))
+time.sleep(milliseconds / 1000)
+sqrt = math.sqrt(num)
+print(sqrt)
